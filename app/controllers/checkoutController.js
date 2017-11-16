ad2d0243b3a29be8f8234313777b157255ea816b
Tiago Carmona <eu@tiagocarmona.com.br>
2017-11-16 04:56:03 -0200

    adicionando a pagina de checkout. Ela ainda não funciona muito bem, mas esta bonita pelo menos

---
+++ app/controllers/checkoutController.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('app');
+
+  app
+  .controller('CheckoutCtrl', ['$scope', '$rootScope', '$routeParams', 'ProductModelService', function($scope, $rootScope, $routeParams, ProductModelService) {
+    $scope.method = 'cartao';
+  }]);
+})();
