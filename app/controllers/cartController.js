3eef6fc6917231524fc4ccc7524b94afe35c8121
Tiago Carmona <eu@tiagocarmona.com.br>
2017-11-15 23:31:28 -0200

    limpando o controller

--- app/controllers/cartController.js
+++ app/controllers/cartController.js
@@ -6,13 +6,6 @@
   app
   .controller('CartCtrl', ['$scope', '$routeParams', 'ProductModelService', function($scope, $routeParams, ProductModelService) {
 
-    $scope.cart = [
-      {"productId" : "5a0370e8f60327670a074edf", "qtd" : 1},
-      {"productId" : "5a03a80df60327670a074ee5", "qtd" : 1},
-    ];
-
-    $scope.products = [];
-
     $scope.getCart = function() {
       angular.forEach($scope.cart, function(value, key){
         ProductModelService.getProductById(value.productId).then(function(product) {
@@ -21,16 +14,9 @@
           $scope.products.push(product);
         });
       });
+      console.log($scope.cart);
+      console.log("cart");
     }
-
-
-    $scope.getProduct = function() {
-      var productId = $routeParams.productId;
-      return ProductModelService.getProductById(productId).then(function(product) {
-        $scope.product = product;
-      });
-    };
-
   }]);
 
 })();
